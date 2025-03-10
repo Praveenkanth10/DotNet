185390ed875911f4ee20be3ab304f09aeba6b8b1
Praveen kanth D <145778653+Praveenkanth10@users.noreply.github.com>
2025-03-10 15:03:22 +0530

    Add files via upload

---
+++ Controllers/JobController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Quartz;
+using System.Threading.Tasks;
+
+[ApiController]
+[Route("[controller]")]
+public class JobController : ControllerBase
+{
+    private readonly ISchedulerFactory _schedulerFactory;
+
+    public JobController(ISchedulerFactory schedulerFactory)
+    {
+        _schedulerFactory = schedulerFactory;
+    }
+
+    [HttpPost("trigger")]
+    public async Task<IActionResult> TriggerJob()
+    {
+        var scheduler = await _schedulerFactory.GetScheduler();
+        var job = JobBuilder.Create<SampleJob>().Build();
+        var trigger = TriggerBuilder.Create().StartNow().Build();
+        await scheduler.ScheduleJob(job, trigger);
+        return Ok("Job triggered successfully.");
+    }
+}
+
---
+++ CronSchedulerProject/Jobs/SampleJob.cs
@@ -0,0 +1,13 @@
+﻿using Quartz;
+using System;
+using System.Threading.Tasks;
+
+public class SampleJob : IJob
+{
+    public async Task Execute(IJobExecutionContext context)
+    {
+        Console.WriteLine($"Job executed at: {DateTime.Now}");
+        await Task.CompletedTask;
+    }
+}
+
